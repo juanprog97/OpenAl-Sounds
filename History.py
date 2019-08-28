99ad543e6ab9e9e57aec5bad1d247be3a806052d
juan <juancard190@gmail.com>
2019-08-27 23:03:17 -0500

    Subida Interface

--- History.py
+++ History.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+from openal import *
+import time
 # Form implementation generated from reading ui file 'History.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
@@ -7,6 +8,132 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+global answer ,situacionPrimera
+
+situaciones = [("nombre","comentario",["]),(),()]
+Decisiones = []
+
+def cargarHistoria():
+    global answer ,situacionPrimera
+    historia = GrafoDeHistoria()
+    nombre = 'prueba1'
+    situacionPrimera = nodoGrafoSituacion(nombre)
+    comentar = "¿Que le gusta Comer?"
+    situacionPrimera.comentario(comentar)
+    sonido = "./sound/prueba.wav"
+    situacionPrimera.agregarSonido(sonido)
+    uno= 'pizza'
+    dos= 'Hamburguesa'
+    tres = 'Salchipapa'
+    situacionPrimera.agregarDecision(uno)
+    situacionPrimera.agregarDecision(dos)
+    situacionPrimera.agregarDecision(tres)
+    situacionPrimera.expoUi()
+
+
+
+
+    #Importar los sonidos-------------------------------------
+
+    #---------------------------------------------------------
+
+class GrafoDeHistoria:
+    def __init__(self):
+        self.grafo = {}
+    def agregarVertice(self,vertice):
+        self.grafo[vertice] = []
+    def eliminarVertice(self, vertice):
+        try:
+            self.grafo.pop(vertice)
+        except KeyError:
+            print("Error")
+    def esVertice(self, vertice):
+        try:
+            self.grafo[vertice]
+            return True
+        except KeyError:
+            return False
+
+    def agregarArco(self, vertice, arco):
+        try:
+            self.grafo[vertice].append(arco)
+        except KeyError:
+            print("Error")
+
+    def eliminarArco(self, vertice, arco):
+        try:
+            self.grafo[vertice].remove(arco)
+        except KeyError:
+            print("Error")
+        except ValueError:
+            print("Error")
+
+    def conseguirArco(self, vertice):
+        try:
+            return self.grafo[vertice]
+        except KeyError:
+            print("Error")
+
+
+class nodoGrafoSituacion:
+    def __init__(self,nom):
+        self.nombre = nom
+        self.comentario
+        self.decisiones= []
+        self.Nm = ['A)','B)','C)','D)']
+        self.audio =""
+        self.bloq = [True,True,True,True]
+        self.nodoHoja = False
+    def comentario(self,comentarioSituacion):
+        self.comentario = comentarioSituacion
+    def agregarDecision(self,decision):
+        self.decisiones.append(decision)
+    def nodoFinal(self):
+        self.nodoHoja = True
+
+    def agregarSonido(self,sonido):
+        self.audio=oalOpen(sonido)
+    def reproducirSonido(self):
+        try:
+            self.audio.play()
+            while self.audio.get_state() == AL_PLAYING:
+	               time.sleep(0)
+            oalQuit()
+        except KeyError:
+            print("Error")
+    def expoUi(self):
+        dec = ""
+        for i in range(len(self.decisiones)):
+            dec += self.Nm[i] + self.decisiones[i]+".\n"
+        print(dec)
+        ui.label.setText(self.comentario+"\n\n"+dec)
+        for i in range(4):
+            if(i<len(self.decisiones)):
+                self.bloq[i] = True
+            else:
+                self.bloq[i] = False
+        ui.pushButton.setEnabled(self.bloq[0])
+        ui.pushButton_2.setEnabled(self.bloq[1])
+        ui.pushButton_3.setEnabled(self.bloq[2])
+        ui.pushButton_4.setEnabled(self.bloq[3])
+
+class nodoGrafoDecision:
+    def __init__(self,nom):
+        self.nombre = nom
+        self.audio = ""
+    def agregarSonido(self,sonido):
+        self.audio= oalOpen(sonido)
+    def reproducirSonido(self):
+        try:
+            self.audio.play()
+            while source.get_state() == AL_PLAYING:
+	               time.sleep(0)
+            oalQuit()
+        except KeyError:
+            print("Error")
+
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -18,7 +145,7 @@ class Ui_MainWindow(object):
         self.groupBox.setGeometry(QtCore.QRect(10, 30, 231, 281))
         self.groupBox.setObjectName("groupBox")
         self.label = QtWidgets.QLabel(self.groupBox)
-        self.label.setGeometry(QtCore.QRect(0, 20, 131, 31))
+        self.label.setGeometry(QtCore.QRect(0, 20, 131, 101))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setEnabled(False)
@@ -72,13 +199,50 @@ class Ui_MainWindow(object):
         self.pushButton_5.setText(_translate("MainWindow", "Continuar"))
         self.label_3.setText(_translate("MainWindow", "Opcion:"))
 
+def botonA():
+
+    answer = 'a'
+
+    print("erda")
+def botonB():
+    global answer ,situacionPrimera
+    answer = 'b'
+    situacionPrimera.reproducirSonido()
+def botonC():
+    global answer
+    answer = 'c'
+def botonD():
+    global answer
+    answer = 'd'
+
+def continuar():
+    global answer
+    answer = ''
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     import sys
+    global answer
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    sys.exit(app.exec_())
+    answer= ""
+    ui.pushButton.clicked.connect(botonA)
+    ui.pushButton_2.clicked.connect(botonB)
+    ui.pushButton_3.clicked.connect(botonC)
+    ui.pushButton_4.clicked.connect(botonD)
 
+    ##Seccion se llena el arbol con la historia
+    cargarHistoria()
+    #while Historia.end() != True:
+
+
+    sys.exit(app.exec_())
